af48f7d2c724c544b40cc1a3edde4f20376b19e8
Rod Widdowson <rdw@steadingsoftware.com>
2016-11-27 15:29:17 +0000

    Checkpoint some auto-build work
    - My working version config.bat (used to set the environment prior to building)
      This includes
         o A couple of extra environmental variables (MsVCVersion, ForceImpotyBeforeCppTargets)
         o A setting of PATH to help run executables
    - Some changes to xsec.props (for Santuario)
         o Abstract the VC version out to config.bat
         o Supress WinCapi in the build
         o Some (probably erroneous) changes in preparation for OpenSSL1.1

--- dependencies/config.bat
+++ dependencies/config.bat
@@ -1,19 +1,25 @@
-set SCRIPTS_DIR=H:\Perforce\sp-dependency-build\scripts
+set SCRIPTS_DIR=H:\Perforce\devel\cpp-msbuild\dependencies
 set VS14_DIR=c:\Program Files (x86)\Microsoft Visual Studio 14.0\
 
 set ZLIB_DIR=zlib-1.2.8
 set ZLIB_IMPLIB=zlib1
 set ZLIB_SHAREDLIB=zlib1_2
 
-set LOGSHIB_DIR=cpp-log4shib-1.9
+set LOGSHIB_DIR=log4shib-1.0.10
 
-set XERCES_DIR=xerces-3.1.99
+set XERCES_DIR=xerces-c-3.1.3
 
-set OPENSSL_DIR=openssl-1.0.2g
-set OPENSSL_NAME=1_0_2_7
+set OPENSSL_DIR=openssl-1.1
+set OPENSSL_NAME=1_0_2_4
 
-set LIBCURL_DIR=curl-7.48.0
+set LIBCURL_DIR=curl-7.49.1
 set LIBCURL_NAME=libcurl5
 
-set XSEC_DIR=xml-security-cpp
+set XSEC_DIR=xml-security-c-1.7.99-X
+
+set MsVCVersion=VC10
+
+set ForceImportBeforeCppTargets=%SCRIPTS_DIR%\xsec.props
+
+set path=%PATH%;H:\Perforce\devel\%OPENSSL_DIR%\out32dll.dbg;H:\Perforce\devel\%XERCES_DIR%\BUILD\WIN32\VC10\DEBUG;H:\Perforce\devel\%XSEC_DIR%\Build\Win32\VC10\Debug No Xalan;H:\Perforce\devel\%libcurl_dir%\lib\debug-dll-ssl-dll-zlib-dll;H:\Perforce\devel\%LOGSHIB_DIR%\msvc10\Debug;H:\Perforce\devel\%ZLIB_DIR%\Debug
 
