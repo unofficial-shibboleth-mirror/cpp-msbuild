119a08dcd82634aef5d5b2126c39e5d54286004d
Rod Widdowson <rdw@steadingsoftware.com>
2016-12-28 17:26:55 +0000

    We need to compile the tools as well

--- dependencies/xmlsec-compile.bat
+++ dependencies/xmlsec-compile.bat
@@ -1,12 +1,15 @@
+rem xmlsec-compile Win64 
+rem xmlsec-compile Win32
+
 set ForceImportBeforeCppTargets=%SCRIPTS_DIR%\xsec.props
 msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug No Xalan"     /t:clean /p:Platform=%1%
 @if errorlevel 1 goto end
 msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release No Xalan"   /t:clean /p:Platform=%1%
 @if errorlevel 1 goto end
 
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug No Xalan"     /t:xsec_lib:Rebuild /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug No Xalan"     /t:Rebuild /p:Platform=%1%
 @if errorlevel 1 goto end
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release No Xalan"   /t:xsec_lib:Rebuild /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release No Xalan"   /t:Rebuild /p:Platform=%1%
 @if errorlevel 1 goto end
 
 
