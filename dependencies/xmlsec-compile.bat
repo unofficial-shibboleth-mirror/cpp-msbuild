3ed22178e2da14308f58ca0a1d12fb5f9cda42f3
Rod Widdowson <rdw@steadingsoftware.com>
2017-09-19 15:34:24 +0100

    CPPXT-123 XMLSecuirty Build changes
    https://issues.shibboleth.net/jira/browse/CPPXT-123
    
    1) Major Version is now 2 (so names and GUIDS change)
    2) The projects we need are now called Minimal, not No Xalan (so paths change)

--- dependencies/xmlsec-compile.bat
+++ dependencies/xmlsec-compile.bat
@@ -2,14 +2,14 @@ rem xmlsec-compile Win64
 rem xmlsec-compile Win32
 
 set ForceImportBeforeCppTargets=%SCRIPTS_DIR%\xsec.props
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug No Xalan" /m    /t:clean /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug Minimal" /m    /t:clean /p:Platform=%1%
 @if errorlevel 1 goto end
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release No Xalan" /m  /t:clean /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release Minimal" /m  /t:clean /p:Platform=%1%
 @if errorlevel 1 goto end
 
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug No Xalan" /m    /t:Build /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Debug Minimal" /m    /t:Build /p:Platform=%1%
 @if errorlevel 1 goto end
-msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release No Xalan" /m  /t:Build /p:Platform=%1%
+msbuild /m projects\VC%MsVCVersionNum%.0\xsec\xsec.sln /p:Configuration="Release Minimal" /m  /t:Build /p:Platform=%1%
 @if errorlevel 1 goto end
 
 
