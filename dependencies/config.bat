3259b2daf6d98a6b9a5d5bd9137c1324d306b524
Scott Cantor <cantor.2@osu.edu>
2017-11-21 12:51:01 -0500

    Settings for my build, update some lib names and versions.

--- dependencies/config.bat
+++ dependencies/config.bat
@@ -1,28 +1,32 @@
 rem GOTTA HAVE TRAILING SLASH
 
-set ROOT_DIR=H:\Perforce\VS2017\
+Rod...
+rem set ROOT_DIR=H:\Perforce\VS2017\
+rem set VS_DIR=d:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\
 
-set VS_DIR=d:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\
+Scott...
+set ROOT_DIR=C:\Users\cantor.2\Shibboleth\
+set VS_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
 
 set ZLIB_DIR=zlib-1.2.11
 set ZLIB_IMPLIB=zlib1
-set ZLIB_SHAREDLIB=zlib1_2
+set ZLIB_SHAREDLIB=zlib1_2_11
 
-set LOGSHIB_DIR=log4shib-1.0.10
+set LOGSHIB_DIR=cpp-log4shib
 
 set XERCES_DIR=xerces-c-3.2.0
 
-set OPENSSL_DIR=openssl-1.1.0f
+set OPENSSL_DIR=openssl-1.1.0g
 
 set SSLLib=libcrypto.lib;libssl.lib
 set SSLLib_space=libcrypto.lib libssl.lib
 set SSLLibD=libcrypto.lib;libssl.lib
 set SSLLibD_space=libcrypto.lib libssl.lib
 
-set LIBCURL_DIR=curl-7.55.1
+set LIBCURL_DIR=curl-7.56.1
 set LIBCURL_NAME=libcurl5
 
-set XSEC_DIR=xml-security-c-1.8.0
+set XSEC_DIR=xml-security-cpp
 
 SET MsVCVersionNum=15
 
