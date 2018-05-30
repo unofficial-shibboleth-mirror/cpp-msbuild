c05a69bcd2d5e9371c884d52eea875edc94d74d4
Scott Cantor <cantor.2@osu.edu>
2018-05-30 16:25:25 -0400

    Fix breaking comments.

--- dependencies/config.bat
+++ dependencies/config.bat
@@ -2,18 +2,18 @@ rem ROOT_DIR HAS TO HAVE TRAILING SLASH
 
 if "%1" NEQ "" goto %1%
 
-#
-# DEFAULT SETTINGS (for Scott)
-#
+REM
+REM DEFAULT SETTINGS (for Scott)
+REM
 
 set ROOT_DIR=C:\Users\scantor\Shibboleth\
 set CPP_MM_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405
 set SED=C:\cygwin64\bin\sed.exe
 rem set DEBUG_INSTALLER=YES
 
-#
-# Apache Config
-#
+REM
+REM Apache Config
+REM
 SET APACHE_13_ROOT=C:\Apache
 SET APACHE_20_ROOT=C:\Apache2.0.63\Apache
 SET APACHE_22_ROOT=C:\Apache2
@@ -29,9 +29,9 @@ set SED=C:\Program Files\Git\usr\bin\sed.exe
 set PERL=C:\Perl64\bin\perl.exe
 rem set DEBUG_INSTALLER=YES
 
-#
-# Apache Config
-#
+REM
+REM Apache Config
+REM
 SET APACHE_13_ROOT=C:\Apache
 SET APACHE_20_ROOT=C:\Apache2.0.63\Apache
 SET APACHE_22_ROOT=C:\Apache2
@@ -48,9 +48,9 @@ set CPP_MM_DIR=D:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\
 set DEBUG_INSTALLER=YES
 
 
-##
-## Apache Config
-##
+REM
+REM Apache Config
+REM
 SET APACHE_13_ROOT=%ROOT_DIR%apache\VC10\Apache
 SET APACHE_20_ROOT=%ROOT_DIR%apache\VC10\Apache2.0.63\Apache2
 SET APACHE_22_ROOT=%ROOT_DIR%apache\VC10\Apache2
@@ -65,9 +65,9 @@ set SED=D:\downloads\cygwin\bin\sed.exe
 set PERL=C:\Strawberry\perl\bin\perl.exe
 set CPP_MM_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Redist\MSVC\14.11.25325\MergeModules
 
-##
-## Apache Config
-##
+REM
+REM Apache Config
+REM
 SET APACHE_13_ROOT=%ROOT_DIR%apache\VC10\Apache
 SET APACHE_20_ROOT=%ROOT_DIR%apache\VC10\Apache2.0.63\Apache2
 SET APACHE_22_ROOT=%ROOT_DIR%apache\VC10\Apache2
@@ -78,56 +78,56 @@ goto path_done
 
 :path_done
 
-##
-## OPENSSL CONFIG
-##
-## OPENSSL_DIR          is the directory name
-## OPENSSL_MM_VERSION   is the Merge Module version (and so have to be 3 or 4 dot separated numbers)
-## OPENSSL_FILE_VERSION is the distinction on the name of the DLLS
-##                      if it changes you *MUST* change Version.props
+REM
+REM OPENSSL CONFIG
+REM
+REM OPENSSL_DIR          is the directory name
+REM OPENSSL_MM_VERSION   is the Merge Module version (and so have to be 3 or 4 dot separated numbers)
+REM OPENSSL_FILE_VERSION is the distinction on the name of the DLLS
+REM                      if it changes you *MUST* change Version.props
 set OPENSSL_DIR=openssl-1.1.0h
 set OPENSSL_MM_VERSION=1.1.0.8
 set OPENSSL_FILE_VERSION=1_1_0
 
-##
-## CURL CONFIG
-##
-## CURL_DIR             is the directory name
-## CURL_VERSION         is the version
-##
+REM
+REM CURL CONFIG
+REM
+REM CURL_DIR             is the directory name
+REM CURL_VERSION         is the version
+REM
 Set LIBCURL_VERSION=7.60.0
 set LIBCURL_DIR=curl-%LIBCURL_VERSION%
 set LIBCURL_FILE_VERSION=7_60
 
-##
-## XML SECURITY CONFIG
-##
-## XSEC_DIR              is the directory name
-## XSEC_MM_VERSION       is the Merge Module version (and so have to be 3 or 4 dot separated numbers)
-## XSEC_FILE_VERSION     is the distinction on the name of the DLLS
-##                       if it changes you *MUST* change Version.props
+REM
+REM XML SECURITY CONFIG
+REM
+REM XSEC_DIR              is the directory name
+REM XSEC_MM_VERSION       is the Merge Module version (and so have to be 3 or 4 dot separated numbers)
+REM XSEC_FILE_VERSION     is the distinction on the name of the DLLS
+REM                       if it changes you *MUST* change Version.props
 set XSEC_DIR=xml-security-cpp
 SET XSEC_FILE_VERSION=2_0
 SET XSEC_MM_VERSION=2.0.0
 
-##
-## ZLIB CONFIG
-##
+REM
+REM ZLIB CONFIG
+REM
 set ZLIB_DIR=zlib-1.2.11
 set ZLIB_MM_VERSION=1.2.11
 set ZLIB_IMPLIB=zlib1
 set ZLIB_FILE_VERSION=1_2_11
 
-##
-## LOG4SHIB CONFIG
-##
+REM
+REM LOG4SHIB CONFIG
+REM
 set LOG4SHIB_DIR=cpp-log4shib
 set LOG4SHIB_MM_VERSION=1.0.10
 set LOG4SHIB_FILE_VERSION=1_0
 
-##
-## XERCES CONFIG
-##
+REM
+REM XERCES CONFIG
+REM
 set XERCES_DIR=xerces-c-3.2.1
 set XERCES_FILE_VERSION=3_2
 set XERCES_MM_VERSION=3.2.1
