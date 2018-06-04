e203045d7346a2a8b77059c0e541d1248721987d
Rod Widdowson <rdw@steadingsoftware.com>
2018-06-04 16:39:38 +0100

    X64/Debug build - make sure that you build resolvertests.exe but not messages.dll

--- dependencies/config.bat
+++ dependencies/config.bat
@@ -45,7 +45,7 @@ goto path_done
 :ROD
 set ROOT_DIR=H:\Perforce\VS2017\
 set SED=sed
-set CPP_MM_DIR=D:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Redist\MSVC\14.13.26020\MergeModules
+set CPP_MM_DIR=D:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Redist\MSVC\14.14.26405\MergeModules
 set DEBUG_INSTALLER=YES
 
 
